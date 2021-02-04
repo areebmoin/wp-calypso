1eeb28482d5b1569f98cb5d243cda2cf48d43bd6
Marco Ciampini <marco.ciampini@automattic.com>
2021-02-04 10:53:49 +0100

    Remove duplicate window types, move correct types to ETK package

--- apps/editing-toolkit/editing-toolkit-plugin/editor-site-launch/src/utils.ts
+++ apps/editing-toolkit/editing-toolkit-plugin/editor-site-launch/src/utils.ts
@@ -5,17 +5,6 @@ import { doAction, hasAction } from '@wordpress/hooks';
 import { addQueryArgs } from '@wordpress/url';
 import { FOCUSED_LAUNCH_FLOW } from './constants';
 
-interface CalypsoifyWindow extends Window {
-	currentSiteId?: number;
-	calypsoifyGutenberg?: {
-		isFocusedLaunchFlow: boolean;
-		isSiteUnlaunched?: boolean;
-		currentCalypsoUrl?: string;
-		[ key: string ]: unknown;
-	};
-}
-declare const window: CalypsoifyWindow;
-
 export const redirectParentWindow = ( url: string ): void => {
 	window.top.location.href = url;
 };
